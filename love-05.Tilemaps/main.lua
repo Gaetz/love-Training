d81239e2aeb34dc549b2f279c1e260d58200b690
Gaetz <gaetan.blaisecazalet@gmail.com>
2018-04-21 19:22:41 +0200

    Tilemap start

---
+++ love-05.Tilemaps/main.lua
@@ -0,0 +1,25 @@
+local windowWidth, windowHeight
+
+
+local numberOfRows = 6
+local numberOfCols = 15
+
+function love.load()
+    windowWidth, windowHeight = love.graphics.getWidth(), love.graphics.getHeight()
+
+end
+
+
+end
+
+function love.update(dt)
+
+end
+
+function love.mousepressed(x, y, n)
+
+end
+
+function love.draw()
+
+end
