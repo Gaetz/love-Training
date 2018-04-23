3890781259bf3286e6ab8c03469d4be2d4238789
Gaetz <gaetan.blaisecazalet@gmail.com>
2018-04-23 10:59:57 +0200

    Hero move and animation

--- love-06.TilemapsCollisions/hero.lua
+++ love-06.TilemapsCollisions/hero.lua
@@ -22,13 +22,71 @@ function hero.load()
     hero.anim = 1
     hero.x = 3
     hero.y = 3
-    hero.isMoving = true
+    hero.targetX = 3
+    hero.targetY = 3
+    hero.isMoving = false
+    hero.speed = 3
 end
 
 function hero.update(dt)
+    -- input
+    if not hero.isMoving then
+        if (love.keyboard.isDown("left") and hero.x > 0) then
+            hero.targetX = hero.x - 1
+            hero.direction = 2
+            hero.isMoving = true
+        end
+        if (love.keyboard.isDown("right") and hero.x < settings.MAP_WIDTH) then
+            hero.targetX = hero.x + 1
+            hero.direction = 3
+            hero.isMoving = true
+        end
+        if (love.keyboard.isDown("down") and hero.x < settings.MAP_HEIGHT) then
+            hero.targetY = hero.y + 1
+            hero.direction = 1
+            hero.isMoving = true
+        end
+        if (love.keyboard.isDown("up") and hero.x > 0) then
+            hero.targetY = hero.y - 1
+            hero.direction = 4
+            hero.isMoving = true
+        end
+    -- move
+    else
+        if hero.x < hero.targetX then
+            hero.x = hero.x + hero.speed * dt
+            if math.floor(hero.x) == hero.targetX then
+                hero.x = hero.targetX
+                hero.isMoving = false
+            end
+        end
+        if hero.x > hero.targetX then
+            hero.x = hero.x - hero.speed * dt
+            if math.ceil(hero.x) == hero.targetX then
+                hero.x = hero.targetX
+                hero.isMoving = false
+            end
+        end
+        if hero.y < hero.targetY then
+            hero.y = hero.y + hero.speed * dt
+            if math.floor(hero.y) == hero.targetY then
+                hero.y = hero.targetY
+                hero.isMoving = false
+            end
+        end
+        if hero.y > hero.targetY then
+            hero.y = hero.y - hero.speed * dt
+            if math.ceil(hero.y) == hero.targetY then
+                hero.y = hero.targetY
+                hero.isMoving = false
+            end
+        end
+    end
+    
+    -- animation
     if hero.isMoving then
         hero.anim = hero.anim + settings.ANIM_SPEED * dt
-        if hero.anim >= 5 then
+        if hero.anim >= #hero.directionTexture[hero.direction] + 1 then
             hero.anim = 1
         end
     else
--- love-06.TilemapsCollisions/settings.lua
+++ love-06.TilemapsCollisions/settings.lua
@@ -4,6 +4,6 @@ settings.SCALE = 5
 settings.TILE_SIZE = 8
 settings.MAP_WIDTH = 10
 settings.MAP_HEIGHT = 10
-settings.ANIM_SPEED = 5
+settings.ANIM_SPEED = 10
 
 return settings
